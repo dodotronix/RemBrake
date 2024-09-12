2f2543ec0bcaefa57a578f923f5794ef0d82da02
dodotronix <dodotronix@gmail.com>
2024-09-12 23:29:29 +0200

    first blinking let demo

--- demo.py
+++ demo.py
@@ -1,8 +1,21 @@
 #!/usr/bin/python
 
 import os
-os.environ.get('BLINKA_FT232H', '1')
+os.environ['BLINKA_FT232H'] = "1"
 
 import board
+import digitalio
 
-print("Hello world")
+from pyftdi.ftdi import Ftdi
+from time import sleep
+
+print(dir(board))
+
+led = digitalio.DigitalInOut(board.C0)
+led.direction = digitalio.Direction.OUTPUT
+
+while True:
+    led.value = True
+    sleep(0.5)
+    led.value = False
+    sleep(0.5)
