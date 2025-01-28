523d2873300eebf0f26a6a374b32613e241aaf45
dodotronix <dodotronix@gmail.com>
2025-01-28 23:39:11 +0100

    updated MY9221 to use tuples to configure each LEDs individually

--- fw/CircuitPython_MY9221.py
+++ fw/CircuitPython_MY9221.py
@@ -1,66 +1,68 @@
 
 # library for MY9221
-from digitalio import DigitalInOut, Direction
+
+from simpleio import DigitalOut
 
 class MY9221():
-    def __init__(self, di, dcki, LedNum=10, intensity=255) -> None:
-        self._di = DigitalInOut(di)
-        self._di.direction = Direction.OUTPUT
-        self._dcki = DigitalInOut(dcki)
-        self._dcki.direction = Direction.OUTPUT
 
-        self._bits = 0x00
-        self._led_num = LedNum
-        self._intensity = intensity
+    WIDTH = 10 # Number of LEDs
 
-    def _my9221_write16(self, data):
-        for i in range(15, -1, -1):
-            self._di.value = (data >> i) & 1
-            state = self._dcki.value
-            self._dcki.value = not state
+    def __init__(self, di, dcki) -> None:
 
-    def _my9221_msg(func):
-        def message(self, *args, **kwargs):
-            # BEGIN
-            self._my9221_write16(0)
-            func(self, *args, **kwargs)
-            # END
-            self._my9221_write16(0)
-            self._my9221_write16(0)
-            # LATCH
-            self._di.value = False
-            for i in range(4):
-                self._di.value = False
-                self._di.value = True
-        return message 
-
-    @_my9221_msg
-    def _refresh_display(self):
-        for i in range(0, self._led_num):
-            word = self._intensity if (self._bits >> i) & 1 else 0
-            self._my9221_write16(word)
+        self._di = DigitalOut(di)
+        self._dcki = DigitalOut(dcki)
+        self.clear()
 
     @property
-    def bits(self):
-        """Internal register to track, which LEDs are ON"""
-        return self._bits
+    def data(self):
+        return self._di.value
 
-    @bits.setter
-    def bits(self, value):
-        self._bits = 0x3ff & value
-        self._refresh_display()
+    @data.setter
+    def data(self, value):
+        self._di.value = value
 
     @property
-    def intensity(self):
-        """Changes the brightness of the LEDs"""
-        return self._intensity
+    def clock(self):
+        return self._dcki.value
 
-    @intensity.setter
-    def intensity(self, value):
-        self._intensity = 0xff & value
-        self._refresh_display()
+    @clock.setter
+    def clock(self, value):
+        self._dcki.value = value
 
-    def get_number_of_segments(self):
-        return self._led_num
+    @property
+    def register(self):
+        return self._register
 
+    @register.setter
+    def register(self, config):
+        if isinstance(config, tuple):
+            id, intensity = config
+            self._register[id] = intensity
+        elif isinstance(config, list):
+            for id, intensity in config:
+                self._register[id] = intensity
+        else:
+            raise ValueError("Value has to be tuple or list of tuples")
 
+        self.refresh()
+
+    def clear(self):
+        self._register = [0] * self.WIDTH  
+
+    def refresh(self):
+        # CREATE MESSAGE
+        words = [0] + self._register + [0, 0] 
+
+        def shift_out(word):
+            """ This copies the bits into the shift register"""
+            for w in f"{word:016b}":
+                self.data = int(w, 2)
+                self.clock = not self.clock
+
+        for w in words:
+            shift_out(w)
+
+        # LATCH
+        self.data = False
+        for i in range(4):
+            self.data = not self.data
