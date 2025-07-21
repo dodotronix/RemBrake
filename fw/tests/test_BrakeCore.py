3faf0a1e8639834ce737bd2af999d1aa7268b272
dodotronix <dodotronix@protonmail.com>
2025-07-21 14:39:45 +0200

    added function for better readability

--- fw/tests/test_BrakeCore.py
+++ fw/tests/test_BrakeCore.py
@@ -21,14 +21,14 @@ class RemBrakeCoreTest(IsolatedAsyncioTestCase):
     def current(self, value):
         self.dut.battery.drv.set_current(value)
 
-    def undervoltage(self):
-        self.dut.battery.drv.status = 0x06 
+    def status(self, value):
+        self.dut.battery.drv.status = value
 
-    def clear_status(self):
-        self.dut.battery.drv.status = 0x00
+    def undervoltage(self):
+        self.status(0x06)
 
     def overflow(self):
-        self.dut.battery.drv.status = 0x20
+        self.status(0x20)
 
     def charged(self, value):
         self.dut.ready.set(value)
@@ -53,55 +53,71 @@ class RemBrakeCoreTest(IsolatedAsyncioTestCase):
 
         asyncio.create_task(self.dut.main())
 
-    async def test_charging(self):
+    # async def test_charging(self):
+    #     async def seq_gen():
+    #         await asyncio.sleep(0.78)
+    #         self.plugged(True)
+    #         await asyncio.sleep(1)
+    #         self.plugged(False)
+    #         await asyncio.sleep(0.2)
+    #         self.plugged(True)
+    #         await asyncio.sleep(1.4)
+
+    #     sequence = asyncio.create_task(
+    #         seq_gen(), name="charging_sequence") 
+    #     await sequence 
+
+    # async def test_reset(self):
+    #     async def seq_gen():
+    #         await asyncio.sleep(1.51)
+    #         self.plugged(True)
+    #         await asyncio.sleep(0.2)
+    #         self.charged(True)
+    #         await asyncio.sleep(1.04)
+    #         self.remote(True)
+    #         await asyncio.sleep(0.2)
+    #         self.handlebars(True)
+    #         await asyncio.sleep(3.1)
+
+    #     sequence = asyncio.create_task(
+    #         seq_gen(), name="reset_sequence") 
+    #     await sequence 
+
+    # async def test_user_braking(self):
+    #     async def seq_gen():
+    #         await asyncio.sleep(1.6)
+    #         self.handlebars(True)
+    #         await asyncio.sleep(0.2)
+    #         self.remote(True)
+    #         await asyncio.sleep(18)
+
+    #     sequence = asyncio.create_task(
+    #         seq_gen(), name="user_braking_sequence") 
+    #     await sequence 
+
+    async def test_low_battery(self):
         async def seq_gen():
-            await asyncio.sleep(0.78)
-            self.plugged(True)
-            await asyncio.sleep(1)
-            self.plugged(False)
-            await asyncio.sleep(0.2)
-            self.plugged(True)
-            await asyncio.sleep(1.4)
-
-        sequence = asyncio.create_task(
-            seq_gen(), name="charging_sequence") 
-        await sequence 
-
-    async def test_alarm(self):
-        async def seq_gen():
-            await asyncio.sleep(0.2)
-
-        sequence = asyncio.create_task(
-            seq_gen(), name="alarm_sequence") 
-        await sequence 
-
-    async def test_reset(self):
-        async def seq_gen():
-            await asyncio.sleep(1.51)
-            self.plugged(True)
-            await asyncio.sleep(0.2)
-            self.charged(True)
-            await asyncio.sleep(1.04)
-            self.remote(True)
-            await asyncio.sleep(0.2)
-            self.handlebars(True)
-            await asyncio.sleep(3.1)
-
-        sequence = asyncio.create_task(
-            seq_gen(), name="reset_sequence") 
-        await sequence 
-
-    async def test_user_braking(self):
-        async def seq_gen():
+            self.dut.msg.charge = 0x00fa
+            self.status(0x04)
             await asyncio.sleep(1.6)
-            self.handlebars(True)
-            await asyncio.sleep(0.2)
-            self.remote(True)
-            await asyncio.sleep(18)
+            self.plugged(True)
+            await asyncio.sleep(1.6)
 
         sequence = asyncio.create_task(
-            seq_gen(), name="user_braking_sequence") 
+            seq_gen(), name="low_battery_sequence") 
         await sequence 
+
+    # async def test_boot(self):
+    #     async def seq_gen():
+    #         self.plugged(True)
+    #         self.status(0x02)
+    #         await asyncio.sleep(1.6)
+    #         self.status(0x00)
+    #         await asyncio.sleep(2)
+
+    #     sequence = asyncio.create_task(
+    #         seq_gen(), name="boot_sequence") 
+    #     await sequence 
 
 if __name__ == '__main__':
     unittest.main(verbosity=2, buffer=False)
